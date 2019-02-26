9cb2cf00d14ea97cff1fec1746b268eeaa9a34be
Samit Koyom <samitkoyom@gmail.com>
2019-02-26 15:08:07 +0700

    add new page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProfilePage } from '../profile/profile';
+import { ServicePage } from '../service/service';
+import { InfoPage } from '../info/info';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +12,20 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  gotoProfile(){
+    // คำสั่งในการเปิด page ใหม่
+    this.navCtrl.push(ProfilePage)
+  }
+
+  gotoService(){
+    // คำสั่งในการเปิด page ใหม่
+    this.navCtrl.push(ServicePage)
+  }
+
+  gotoInfo(){
+    this.navCtrl.push(InfoPage)
   }
 
 }
